434aff2345d726a783dfea6758bd080e3e178410
Allan Sacramento <allan-sacramento@outlook.com.br>
2019-08-04 23:42:21 -0300

    first commit

---
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { RestaurantsService } from 'app/restaurants/restaurants.service';
+import { Restaurant } from 'app/restaurants/restaurant/restaurant.model';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'mt-restaurant-detail',
+  templateUrl: './restaurant-detail.component.html'
+})
+export class RestaurantDetailComponent implements OnInit {
+
+  constructor(private restaurantServices: RestaurantsService, private route: ActivatedRoute) { }
+
+  restaurant: Restaurant
+
+  ngOnInit() {
+
+    this.restaurantServices.restaurantById(this.route.snapshot.params['id'])
+      .subscribe(restaurant => this.restaurant = restaurant )
+
+  }
+
+}
